94e7cbd7536c9ba76f0f61c43b8484c4ce40d5c3
Fraktalia <g.w.koscielniak@gmail.com>
2020-09-24 18:48:22 +0200

    Drobne zmiany.

--- secure-erp-python-xtarzyk/model/data_manager.py
+++ secure-erp-python-xtarzyk/model/data_manager.py
@@ -2,15 +2,6 @@
 
 
 def read_table_from_file(file_name, separator=';'):
-    """Read CSV file into a data table.
-
-    Args:
-        file_name: The name of the CSV data file.
-        separator: The CSV separator character
-
-    Returns:
-        The data parsed into a list of lists.
-    """
     try:
         with open(file_name, "r") as file:
             lines = file.readlines()
@@ -20,14 +11,8 @@ def read_table_from_file(file_name, separator=';'):
 
 
 def write_table_to_file(file_name, table, separator=';'):
-    """Write tabular data into a CSV file.
-
-    Args:
-        file_name: The name of the file to write to.
-        table: list of lists containing tabular data.
-        separator: The CSV separator character
-    """
-    with open(file_name, "w") as file:
+    with open(file_name, "a") as file:
+        file.write("\n")
         for record in table:
             row = separator.join(record)
-            file.write(row + "\n")
+            file.write(record + ";")
